5e0d89af0620118577dcc9649516d14541236924
zhangyage <zhangyage2015@163.com>
2017-10-23 20:43:33 +0800

    视图-路由文件

--- movie/app/home/views.py
+++ movie/app/home/views.py
@@ -48,3 +48,12 @@ def animation():
     return render_template("home/animation.html")
 
 
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
+
+
