815bc5ee889cfd98cec93f03e827568b65195c51
zhangyage <zhangyage2015@163.com>
2017-10-23 16:15:48 +0800

    视图路由文件

--- movie/app/admin/views.py
+++ movie/app/admin/views.py
@@ -3,3 +3,8 @@
 '''
     @视图处理模块
 '''
+from . import  admin
+
+@admin.route("/")
+def index():
+    return "<h1 style='color:red'>this is admin</h1>"
--- movie/app/home/views.py
+++ movie/app/home/views.py
@@ -3,3 +3,22 @@
 '''
     @视图处理模块
 '''
+from . import  home
+from flask import render_template,redirect,url_for
+
+@home.route("/")
+def index():
+    return render_template("home/index.html")
+
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
+
+@home.route("/register/")
+def register():
+    return render_template("home/register.html")
+
