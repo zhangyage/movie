f32d47a2cb7ec036c1d37a51e597479205c66482
zhangyage <zhangyage2015@163.com>
2017-10-24 10:11:26 +0800

    ..

--- movie/app/admin/views.py
+++ movie/app/admin/views.py
@@ -4,7 +4,16 @@
     @视图处理模块
 '''
 from . import  admin
+from flask import render_template,url_for,redirect
 
 @admin.route("/")
 def index():
     return "<h1 style='color:red'>this is admin</h1>"
+
+@admin.route("/logout/")
+def logout():
+    return redirect( url_for("admin.login"))
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
