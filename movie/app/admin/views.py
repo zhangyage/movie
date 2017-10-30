c21b040ccfe5555c199e9e4be60f621dc760f394
zhangyage <zhangyage2015@163.com>
2017-10-30 16:18:36 +0800

    视图

--- movie/app/admin/views.py
+++ movie/app/admin/views.py
@@ -5,7 +5,7 @@
 '''
 from . import  admin
 from flask import render_template,url_for,redirect,flash,session,request
-from app.admin.forms import LoginForm,TagForm,MovieForm
+from app.admin.forms import LoginForm,TagForm,MovieForm,PreviewForm
 from app.models import Admin,Tag,User,Adminlog,Auth,Role,Preview,Oplog,Movie,Moviecol,Comment
 from functools import wraps
 from app import db,app
@@ -137,6 +137,10 @@ def movie_add():
     form = MovieForm()
     if form.validate_on_submit():
         data = form.data
+        movie_count = Movie.query.filter_by(title=data["title"]).count() #不可以有重复标签
+        if movie_count == 1:
+            flash("对应的影片已存在！",'err')
+            return redirect(url_for('admin.preview_add'))
         file_url = secure_filename(form.url.data.filename)
         file_logo = secure_filename(form.logo.data.filename)
         '''form.logo.data.filename  获取上传文件文件名'''
@@ -175,7 +179,7 @@ def movie_list(page = None):
         page = 1
     page_data = Movie.query.join(Tag).filter(Tag.id == Movie.tag_id).order_by(
         Movie.addtime.asc()
-    ).paginate(page=page, per_page=10)   #paginate分页 (page页码,per_page条目数)
+    ).paginate(page=page, per_page=1)   #paginate分页 (page页码,per_page条目数)
     return render_template("admin/movie_list.html",page_data=page_data)
 
 
@@ -190,30 +194,183 @@ def movie_del(id=None):
     return redirect(url_for('admin.movie_list',page=1))
 
 
-@admin.route("/preview/add/")
+#修改电影
+@admin.route("/movie/update/<int:id>/",methods=["GET","POST"])
+@check_login
+def movie_update(id):
+    form = MovieForm()
+    form.url.validators = []   #不修改这个值，直接设置为空
+    form.logo.validators = []
+    movie = Movie.query.get_or_404(id)
+    if request.method == "GET":
+        form.info.data = movie.info
+        form.tag_id.data = movie.tag_id
+        form.star.data = movie.star
+    if form.validate_on_submit():
+        data = form.data
+        movie_count = Movie.query.filter_by(title=data["title"]).count()
+        if movie_count == 1 and movie.title != data["title"]:
+            flash("修改失败",'err')
+            return redirect(url_for('admin.movie_update',id=id))
+        
+        if form.url.data.filename != "":   #判断我们是否更改图片了  空就是没有更改
+            file_url = secure_filename(form.url.data.filename)
+            movie.url = change_filename(file_url)  
+            form.url.data.save(app.config["UP_DIR"] + movie.url)  
+        if form.logo.data.filename != "":
+            file_logo = secure_filename(form.logo.data.filename)
+            movie.logo = change_filename(file_logo)
+            form.logo.data.save(app.config["UP_DIR"] + movie.logo)
+        
+        movie.title = data["title"]
+        movie.info = data["info"]
+        movie.star = data["star"]
+        movie.tag_id = data["tag_id"]
+        movie.area = data["area"]
+        movie.release_time = data["release_time"]
+        movie.length = data["length"]
+        db.session.add(movie)
+        db.session.commit()
+        flash("修改电影成功",'OK')
+        return redirect(url_for('admin.movie_update',id=id))
+    return render_template("admin/movie_update.html",form=form,movie=movie)  #movie=movie传进初始值，方便我们参考修改
+
+
+#预告添加
+@admin.route("/preview/add/",methods=["GET","POST"])
 @check_login
 def preview_add():
-    return render_template("admin/preview_add.html")
+    form = PreviewForm()
+    if form.validate_on_submit():
+        data = form.data
+        preview_count = Preview.query.filter_by(title=data["title"]).count() #不可以有重复标签
+        if preview_count == 1:
+            flash("对应的预告已存在！",'err')
+            return redirect(url_for('admin.preview_add'))
+        file_logo = secure_filename(form.logo.data.filename)
+        '''form.logo.data.filename  获取上传文件文件名'''
+        if not os.path.exists(app.config["UP_DIR"]):  #判断长传目录是否存在
+            os.makedirs(app.config["UP_DIR"])
+            os.chmod(app.config["UP_DIR"], "rw")
+        logo = change_filename(file_logo)
+        form.logo.data.save(app.config["UP_DIR"] + logo)
+        preview = Preview(
+            title = data["title"],
+            logo = logo
+            )
+        db.session.add(preview)
+        db.session.commit()
+        flash("预告电影添加成功",'OK')
+        return redirect(url_for('admin.preview_add'))
+    return render_template("admin/preview_add.html",form = form)
 
-@admin.route("/preview/list/")
+#预告列表
+@admin.route("/preview/list/<int:page>/",methods=["GET"])
 @check_login
-def preview_list():
-    return render_template("admin/preview_list.html")
+def preview_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Preview.query.filter_by().order_by(
+        Preview.addtime.asc()
+    ).paginate(page=page, per_page=10)   #paginate分页 (page页码,per_page条目数)
+    return render_template("admin/preview_list.html",page_data=page_data)
 
-@admin.route("/user/list/")
+#删除预告
+@admin.route("/preview/del/<int:id>/",methods=["GET"])
 @check_login
-def user_list():
-    return render_template("admin/user_list.html")
+def preview_del(id=None):
+    preview=Preview.query.filter_by(id=id).first_or_404()   #如果没有返回404
+    db.session.delete(preview)
+    db.session.commit()
+    flash("删除预告成功", "OK")
+    return redirect(url_for('admin.preview_list',page=1))
 
-@admin.route("/user/view/")
-@check_login
-def user_view():
-    return render_template("admin/user_view.html")
 
-@admin.route("/comment/list/")
+#修改预告电影
+@admin.route("/preview/update/<int:id>/",methods=["GET","POST"])
 @check_login
-def comment_list():
-    return render_template("admin/comment_list.html")
+def preview_update(id):
+    form = PreviewForm()
+    form.logo.validators = []   #不修改这个值，直接设置为空
+    preview = Preview.query.get_or_404(id)
+    if form.validate_on_submit():
+        data = form.data
+        preview_count = Preview.query.filter_by(title=data["title"]).count()
+        if preview_count == 1 and preview.title != data["title"]:
+            flash("修改失败！对应的预告片已经存在",'err')
+            return redirect(url_for('admin.preview_update',id=id))
+        if form.logo.data.filename != "": #判断我们是否更改图片了  空就是没有更改
+            file_logo = secure_filename(form.logo.data.filename)
+            preview.logo = change_filename(file_logo)
+            form.logo.data.save(app.config["UP_DIR"] + preview.logo)
+        
+        preview.title = data["title"]
+        db.session.add(preview)
+        db.session.commit()
+        flash("预告电影修改成功",'OK')
+        return redirect(url_for('admin.preview_update',id=id))
+    return render_template("admin/preview_update.html",form=form,preview=preview)  #movie=movie传进初始值，方便我们参考修改
+
+
+#会员列表
+@admin.route("/user/list/<int:page>/",methods=["GET"])
+@check_login
+def user_list(page=None):
+    if page is None:
+        page = 1
+    page_data = User.query.filter_by().order_by(
+        User.addtime.asc()
+    ).paginate(page=page, per_page=10)   #paginate分页 (page页码,per_page条目数)
+    return render_template("admin/user_list.html",page_data=page_data)
+
+
+#会员查看
+@admin.route("/user/view/<int:id>",methods=["GET"])
+@check_login
+def user_view(id=None):
+    user = User.query.get_or_404(int(id))
+    return render_template("admin/user_view.html",user=user)
+
+
+#删除会员
+@admin.route("/user/del/<int:id>/",methods=["GET"])
+@check_login
+def user_del(id=None):
+    user=User.query.filter_by(id=id).first_or_404()   #如果没有返回404
+    db.session.delete(user)
+    db.session.commit()
+    flash("删除预告成功", "OK")
+    return redirect(url_for('admin.user_list',page=1))
+
+
+#评论列表
+@admin.route("/comment/list/<int:page>",methods=["GET"])
+@check_login
+def comment_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Comment.query.join(
+            Movie
+        ).join(
+            User
+        ).filter(
+           Movie.id == Comment.movie_id,
+           User.id == Comment.user_id 
+        ).order_by(
+        User.addtime.asc()
+    ).paginate(page=page, per_page=10)   #paginate分页 (page页码,per_page条目数)
+    return render_template("admin/comment_list.html",page_data=page_data)
+
+
+#删除评论
+@admin.route("/comment/del/<int:id>/",methods=["GET"])
+@check_login
+def comment_del(id=None):
+    comment=Comment.query.filter_by(id=id).first_or_404()   #如果没有返回404
+    db.session.delete(comment)
+    db.session.commit()
+    flash("评论删除成功", "OK")
+    return redirect(url_for('admin.comment_list',page=1))
 
 @admin.route("/moviecol/list/")
 @check_login
