6ceb74c2ca87bf7a8b93d2a9a5ab95fab2b8c616
zhangyage <zhangyage2015@163.com>
2017-11-06 14:39:45 +0800

    ..

--- movie/app/__init__.py
+++ movie/app/__init__.py
@@ -1,6 +1,7 @@
 # coding:utf8
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_redis import FlaskRedis
 import pymysql
 import os
 
@@ -8,11 +9,13 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://zhangyage:Zhang123@47.94.188.237/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = 'af2fad8cfe1f4c5fac4aa5edf6fcc8f3'
+app.config["REDIS_URL"] = "redis://192.168.75.7:6379/0"
 #配置文件上传路径
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/uploads/")
 
 app.debug = True
 db = SQLAlchemy(app)
+rd = FlaskRedis(app)
 
 
 from app.home import home as home_blueprint
